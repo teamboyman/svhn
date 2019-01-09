15de3d35cfc281a3105e0ed7d9332c39bae6617c
teamboyman <teamboyman@gmail.com>
2019-01-09 07:32:10 +0700

    Update inference.py

--- inference.py
+++ inference.py
@@ -4,8 +4,8 @@ import math
 import numpy as np
 from PIL import Image
 
-from digits_inference import DigitsInference
-from inference_wrapper import InferenceWrapper
+from svhn.digits_inference import DigitsInference
+from svhn.inference_wrapper import InferenceWrapper
 
 
 class Inference(object):
